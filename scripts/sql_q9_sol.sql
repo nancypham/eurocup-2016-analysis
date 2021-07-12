f980860bf3fcd8e2d504db6c1359c345d25b2b07
Nancy Pham <16923131+nancypham@users.noreply.github.com>
2021-07-12 12:11:30 -0400

    improve sql query 9

--- scripts/sql_q9_sol.sql
+++ scripts/sql_q9_sol.sql
@@ -9,4 +9,4 @@ WITH germany_country_id AS (
 SELECT DISTINCT p.player_name AS german_goalkeeper, p.jersey_no
 FROM player_mast AS p, soccer_country AS s, germany_country_id as g
 WHERE posi_to_play = 'GK' AND
-p.player_id = (SELECT DISTINCT player_gk FROM match_details AS m, germany_country_id AS g WHERE play_stage = 'G' AND m.team_id = g.country_id);
+p.player_id in (SELECT DISTINCT player_gk FROM match_details AS m, germany_country_id AS g WHERE play_stage = 'G' AND m.team_id = g.country_id);
